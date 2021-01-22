d4e90d2f0983cfcd2daea4d8207b7ef8ef3099d3
zhanghaoyu020418 <zhanghaoyu020418@yeah.net>
2021-01-22 19:51:24 +0800

    2021-1-22

---
+++ winter_1_22/winter_1_22/dfs.c
@@ -0,0 +1,131 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//void print(int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+////int arr[5] = { 0 };
+////int i = 0;
+////while (num)
+////{
+////	arr[i] = num % 10;
+////	num /= 10;
+////	i++;
+////}
+//
+////显然不用递归会很麻烦，而且要规定数组的大小，否则会栈溢出
+//
+//void fun1()
+//{
+//	//将1234，打印出来1 2 3 4
+//	int num;
+//	scanf("%d", &num);
+//	print(num);
+//}
+////*****************************************************
+//
+////第一种，利用临时变量计数
+//int my_strlen1(char *arr)
+//{
+//	int cnt = 0;
+//	while (0 != *arr)
+//	{
+//		cnt++;
+//		arr++;
+//	}
+//	return cnt;
+//}
+//
+////可不可以不用临时变量？？？
+//
+////第二种，递归
+//int my_strlen2(char* arr)
+//{
+//	if (0 != *arr)
+//		return my_strlen2(arr + 1) + 1;
+//	else
+//		return 0;
+//}
+//
+//void fun2()
+//{
+//	//strlen的实现过程
+//	char arr[] = "zhanghaoyu";
+//	int ret = my_strlen2(arr);
+//
+//	printf("%d", ret);
+//}
+//
+//
+//int Fac(int num)
+//{
+//	if (num <= 1)
+//		return 1;
+//	else
+//		return num * Fac(num - 1);
+//}
+//
+//void fun3()
+//{
+//	//n的阶乘
+//	int n;
+//	while (scanf("%d", &n))
+//	{
+//		long long t = Fac(n);
+//		printf("%d\n", t);
+//	}
+//
+//}
+//
+//
+////用递归写斐波那契数列有明显的劣势，每次都要大量的重复计算，效率很低
+//int Fib(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return Fib(n - 1) + Fib(n - 2);
+//}
+//
+//void fun4()
+//{
+//	int num;
+//	while (scanf("%d", &num))
+//	{
+//		int t = Fib(num);
+//		printf("%d\n", t);
+//
+//	}
+//}
+//
+//void fun5()
+//{
+//	int num;
+//	while (scanf("%d", &num))
+//	{
+//		int a = 1;
+//		int b = 1;
+//		int c = 0;
+//
+//		while (num - 2)
+//		{
+//			c = a + b;
+//			a = b;
+//			b = c;
+//			num--;
+//		}
+//		printf("%d\n", c);
+//	}
+//}
+//
+//
+//int main()
+//{
+//
+//	return 0;
+//}
---
+++ winter_1_22/winter_1_22/汉诺塔问题（面试题）.c
@@ -0,0 +1,34 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//static int cnt = 0;
+//
+//void move(char a, char c)
+//{
+//	cnt++;                      //每一移动就cnt+1,最后可以看到移动的次数
+//}
+//
+//void hanoi(int n, char a, char b, char c)
+//{
+//	if (n == 1)                 //如果只有个板
+//	{
+//		move(a, c);             //直接将板从 第一个杆子 移动到 第三个杆子
+//	}
+//	else                        //如果有>=2个板
+//	{
+//		hanoi(n - 1, a, c, b);  //将除最大板之外的其余板，从杆子1移到杆子2
+//		move(a, c);             //将板从 第一个杆子 移动到 第三个杆子
+//		hanoi(n - 1, b, a, c);  //将除最大板之外的其余板，从杆子2移到杆子3
+//	}
+//}
+//
+//
+//int main()
+//{
+//	int num;
+//	scanf("%d", &num);       //num表示铜板的数量
+//
+//	hanoi(num, 'A', 'B', 'C');//ABC表示三个杆子
+//
+//	printf("%d", cnt);
+//	return 0;
+//}

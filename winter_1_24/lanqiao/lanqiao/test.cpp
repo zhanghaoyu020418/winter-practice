54b971cbf572cd9ee5f32fffbd2bb9e6b4290d59
zhanghaoyu020418 <zhanghaoyu020418@yeah.net>
2021-01-24 22:41:46 +0800

    pratice

--- winter_1_24/lanqiao/lanqiao/test.cpp
+++ winter_1_24/lanqiao/lanqiao/test.cpp
@@ -1,35 +1,35 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
-//#include <iostream>
-//
-//using namespace std;
-//
-//const int N = 35;
-//
-//int main()
-//{
-//	int arr[N][N];
-//	int n;
-//
-//	cin >> n;                               //输入行数
-//	int i, j;
-//
-//	for (i = 0; i < n; i++)                 //从0开始，为了让三角形从第一行打印
-//	{
-//		arr[i][0] = 1;                      //让三角形的第一列为1
-//		arr[i][i] = 1;                      //让三角形的对角线为1
-//		for (j = 1; j <= i - 1; j++)        //从第三行开始三角形就有变化了
-//		{
-//			arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];//中间的数值= 上面一个数字+左上一个数字
-//		}
-//	}
-//
-//	for (i = 0; i < n; i++)
-//	{
-//		for (j = 0; j <= i; j++)
-//		{
-//			cout << arr[i][j] << " ";
-//		}
-//		cout << endl;
-//	}
-//
-//	return 0;}
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+
+using namespace std;
+
+const int N = 35;
+
+int main()
+{
+	int arr[N][N];
+	int n;
+
+	cin >> n;                               //输入行数
+	int i, j;
+
+	for (i = 0; i < n; i++)                 //从0开始，为了让三角形从第一行打印
+	{
+		arr[i][0] = 1;                      //让三角形的第一列为1
+		arr[i][i] = 1;                      //让三角形的对角线为1
+		for (j = 1; j <= i - 1; j++)        //从第三行开始三角形就有变化了
+		{
+			arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];//中间的数值= 上面一个数字+左上一个数字
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;}
--- winter_1_24/lanqiao/lanqiao/特殊回文数.cpp
+++ winter_1_24/lanqiao/lanqiao/特殊回文数.cpp
@@ -39,3 +39,44 @@
 //
 //	return 0;
 //}
+
+//±©Á¦½â·¨
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int input;
+	cin >> input;
+
+	int i = 0;
+	for (i = 10000; i < 100000; i++)
+	{
+		int a, b, c, d, e;
+		a = i % 10;
+		b = i / 10 % 10;
+		c = i / 100 % 10;
+		d = i / 1000 % 10;
+		e = i / 10000;
+		if (a == e && b == d && input == a + b + c + d + e)
+			cout << i << endl;
+	}
+
+	for (i = 100000; i < 1000000; i++)
+	{
+		int a, b, c, d, e, f;
+		a = i % 10;
+		b = i / 10 % 10;
+		c = i / 100 % 10;
+		d = i / 1000 % 10;
+		e = i / 10000 % 10;
+		f = i / 100000;
+
+		if (a == f && b == e && c == d && input == a + b + c + d + e + f)
+			cout << i << endl;
+	}
+
+
+	return 0;
+}

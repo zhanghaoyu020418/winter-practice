7ee5612710c2870ffdf046fe3a73edd9cfd574af
zhanghaoyu020418 <zhanghaoyu020418@yeah.net>
2021-02-25 23:28:56 +0800

    21-2-25

---
+++ 21-2-25/21-2-25/demo.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	while (cin >> a)
+	{
+		cout << log(a);
+		getchar();
+	}
+
+	return 0;
+}
+

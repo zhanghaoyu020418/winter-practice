bf6d021cf26fe8ca0f4aa1e3fb2dd9c9d73a4d15
zhanghaoyu020418 <zhanghaoyu020418@yeah.net>
2021-03-06 22:19:47 +0800

    213-6

---
+++ 21-3-6/21-3-6/demo.cpp
@@ -0,0 +1,42 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	//string str1, str2;
+//	//cin >> str1 >> str2;
+//	//str1.insert(str1.size(), str2);
+//	//string str = "zhy";
+//
+//	//string sstr = str.insert(0, 2, '6');
+//
+//	//cout << sstr << endl;  
+//
+//	//string str = "zhy";
+//	//str.insert(0, 2, '1');
+//	//cout << sstr << endl;
+//
+//	//string str = "zhy";
+//
+//	//string sstr = str.insert(0, "hello ");
+//
+//	//cout << sstr << endl;
+//
+//	//string str = "zhy";
+//
+//	//str.insert(0, "hello ");
+//
+//	//cout << str << endl;
+//
+//	string str;
+//
+//	char a = str[4];
+//
+//	cout << a << endl;
+//
+//
+//	return 0;
+//}

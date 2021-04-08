328271b4de37c0c1de7a0731c7fd0e8c76b652bd
zhanghaoyu020418 <zhanghaoyu020418@yeah.net>
2021-04-08 22:48:55 +0800

    21-4-8

---
+++ 21-4-8/21-4-8/demo.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+lass Solution{
+public:
+	int findMin(vector<int>& nums) {
+		if (nums.size() == 0) return 0;
+		if (nums[nums.size() - 1] > nums[0]) return nums[0];
+		int l = 0, r = nums.size() - 1;
+		while (l < r) {
+			int mid = l + r >> 1;
+			if (nums[mid] <= nums.back()) r = mid;
+			else l = mid + 1;
+		}
+		return nums[l];
+	}
+};

daf5393e139c16009740b6f9b41270da67bc97a6
zhanghaoyu020418 <zhanghaoyu020418@yeah.net>
2021-05-26 21:35:46 +0800

    21-5-26

--- 21-5-24/21-5-24/demo.cpp
+++ 21-5-24/21-5-24/demo.cpp
@@ -1,28 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
 #include <stack>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> inorderTraversal(TreeNode* root) {
-	stack<TreeNode*> sk;
-	vector<int> ans;
-	while (root != nullptr || !sk.empty()) {
-		while (root != nullptr) {
-			sk.push(root);
-			root = root->left;
-		}
-		root = sk.top();
-		sk.pop();
-		ans.push_back(root->val);
-		root = root->right;
-	}
-	return ans;
-}
+//vector<int> inorderTraversal(TreeNode* root) {
+//	stack<TreeNode*> sk;
+//	vector<int> ans;
+//	while (root != nullptr || !sk.empty()) {
+//		while (root != nullptr) {
+//			sk.push(root);
+//			root = root->left;
+//		}
+//		root = sk.top();
+//		sk.pop();
+//		ans.push_back(root->val);
+//		root = root->right;
+//	}
+//	return ans;
+//}
 
 int main() 
-{
+{/*
 	stack<int> sk;
+	double a(3.14);
+	cout << a << endl;*/
+	double a = 10.1, b = 20.2;
+	swap(a, b);
+	cout << a << ' ' << b << endl;
 
 	return 0;
 }
--- 21-5-25/21-5-25/demo.cpp
+++ 21-5-25/21-5-25/demo.cpp
@@ -11,14 +11,16 @@ int main()
 	//for (int i = 1; i <= 10; i++)
 	//	res.push_back(i * 10);
 	//ans.push_back(res);
-	const int a = 10;
-	int b = 20;
-	//a = b;// 权限的缩小
-	b = a;// 权限的扩大
-	vector<int> a(10);
-	cout << a[10] << endl;
-	cout << b << endl;
-	b = 20;
-	cout << b << endl;
+	//const int a = 10;
+	//int b = 20;
+	////a = b;// 权限的缩小
+	//b = a;// 权限的扩大
+	//vector<int> a(10);
+	//cout << a[10] << endl;
+	//cout << b << endl;
+	//b = 20;
+	//cout << b << endl;
+	int a = 1;
+	cout << a << endl;
 	return 0;
 }

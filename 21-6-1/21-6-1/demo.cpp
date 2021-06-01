47344415073fc2821be5df2d7a039edd1b82d423
zhanghaoyu020418 <zhanghaoyu020418@yeah.net>
2021-06-01 22:37:07 +0800

    21-6-1

---
+++ 21-6-1/21-6-1/demo.cpp
@@ -0,0 +1,25 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//class A
+//{
+//public:
+//	/*explicit */A(int n) : _n(n) { cout << "A(int n)" << endl; }
+//	A(const A& obj)
+//	{
+//		this->_n = obj._n;
+//		cout << "hello" << endl;
+//	}
+//private:
+//	int _n;
+//};
+//
+//int main()
+//{
+//	const A& a = 1;
+//	const string& str = "zhy";
+//	return 0;
+//}
---
+++ 21-6-1/21-6-1/demo1.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+	A(int a = 1)
+	{
+
+	}
+private:
+	int _a = 0;
+};
+
+int main()
+{
+	A a;
+	return 0;
+}
